0b0e05f2f60b2d33e67695a77191d1edf63bb81e
May Lin <40640630+maymlin@users.noreply.github.com>
2023-03-06 20:51:13 -0600

    Implement ScheduleController#getScheduleForEmployee - add ScheduleService#getScheduleForEmployee

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/service/ScheduleService.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/service/ScheduleService.java
@@ -1,2 +1,49 @@
-package com.udacity.jdnd.course3.critter.service;public class ScheduleService {
+package com.udacity.jdnd.course3.critter.service;
+
+import com.udacity.jdnd.course3.critter.pet.Pet;
+import com.udacity.jdnd.course3.critter.repository.ScheduleRepository;
+import com.udacity.jdnd.course3.critter.schedule.Schedule;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ScheduleService {
+
+    @Autowired
+    private ScheduleRepository scheduleRepository;
+    @Autowired
+    private EmployeeService employeeService;
+    @Autowired
+    private PetService petService;
+
+    public Schedule saveSchedule(Schedule schedule) {
+        return scheduleRepository.save(schedule);
+    }
+
+    public Schedule findSchedule(long scheduleId) {
+        return scheduleRepository.findById(scheduleId).get();
+    }
+
+    public List<Schedule> getAllSchedules() {
+        return scheduleRepository.findAll();
+    }
+
+    public List<Schedule> getScheduleForPet(long petId) {
+        return getAllSchedules().stream()
+                .filter(schedule ->
+                        schedule.getPets().contains(petService.findPet(petId)))
+                .collect(Collectors.toList());
+    }
+
+    public List<Schedule> getScheduleForEmployee(long employeeId) {
+        return getAllSchedules().stream()
+                .filter(schedule ->
+                        schedule.getEmployees().contains(employeeService.findEmployee(employeeId)))
+                .collect(Collectors.toList());
+    }
+
 }
